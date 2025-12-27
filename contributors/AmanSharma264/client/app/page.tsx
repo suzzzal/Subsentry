910ee5968c8e4fb9d8110fb1edf37e4e13d3577a
amansharma264 <amankumar170789@gmail.com>
2025-12-27 23:37:10 +0530

    Refactor documentation link text and update .gitignore for Clerk configuration

--- contributors/AmanSharma264/client/app/page.tsx
+++ contributors/AmanSharma264/client/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Doc
           </a>
         </div>
       </main>
